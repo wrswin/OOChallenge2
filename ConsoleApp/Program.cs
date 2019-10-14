57d95681282d55b8f4e883169b682bdcfd36785a
WAYDE REITSMA <102506005@student.swin.edu.au>
2019-10-14 15:16:40 +1100

    Add console app project

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace OOChallenge2.ConsoleApp {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
